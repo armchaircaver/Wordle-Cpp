63acb24222a8c914e36659c75433b4503162a3af
Arthur's Laptop <arthur.vause@gmail.com>
2022-05-25 10:22:52 +0100

    Read solution and guess words from files

--- optimal tree generator/wordlists.cpp
+++ optimal tree generator/wordlists.cpp
@@ -1,17 +1,54 @@
 #include "wordlists.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+std::vector<std::string> read_csv(std::string filename) {
+    // Reads a single line CSV file into a vector of string
+
+  
+    // Create an input filestream
+    std::ifstream myFile(filename);
+
+    // Make sure the file is open
+    if (!myFile.is_open()) throw std::runtime_error("Could not open file");
+
+    // Helper vars
+    std::string line, word;
+ 
+    std::vector<std::string> result;
+
+    // Read the first line
+    if (myFile.good())
+    {
+        // Extract the first line in the file
+        std::getline(myFile, line);
+
+        // Create a stringstream from line
+        std::stringstream ss(line);
+
+        // Extract each word for the line
+        while (std::getline(ss, word, ',')) {
+             result.push_back(word);
+        }
+    }
+ 
+    // Close file
+    myFile.close();
+
+    return result;
+}
 
 PrimaryWords::PrimaryWords() {
 
     if (solutions.size() == 0) {
+ 
+        solutions = read_csv("solutions.txt");
+        alloptions = read_csv("guesses.txt");
 
-        for (int i = 0; i < sizeof(solutions_c) / sizeof(solutions_c[0]); i++)
-            solutions.push_back(solutions_c[i]);
+        alloptions.insert(alloptions.end(), solutions.begin(), solutions.end());
 
         printf("solutions vector initialised, %zd items\n", solutions.size());
-
-        for (int i = 0; i < sizeof(alloptions_c) / sizeof(alloptions_c[0]); i++)
-            alloptions.push_back(alloptions_c[i]);
-
         printf("alloptions vector initialised, %zd items\n", alloptions.size());
     }
 
